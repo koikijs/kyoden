b9b9c6e05308fc8dc7c261bf602bc6b987421a38
sideroad <sideroad.jp@gmail.com>
2017-12-03 12:34:33 +0900

    Show transfer amounts

--- src/components/TransferPayments.jsx
+++ src/components/TransferPayments.jsx
@@ -4,15 +4,23 @@ import PropTypes from 'prop-types';
 const styles = require('../css/transfer-payments.less');
 
 const TransferPayments = props =>
-  <div
-    className={styles.transferPayments}
+  <table
+    className={styles.table}
   >
-    <ul>
-      {props.transferAmounts.map(amount =>
-        <li>{amount.amount}</li>
-      )}
-    </ul>
-  </div>;
+    <tbody>
+      {
+        props.transferAmounts
+          .filter(amount => amount.amount)
+          .map(amount =>
+            <tr key={`${amount.from}-${amount.to}`} >
+              <td className={styles.from} >{amount.from}</td>
+              <td className={styles.to} >{amount.to}</td>
+              <td className={styles.amount} >￥{amount.amount.toLocaleString()}</td>
+            </tr>
+          )
+      }
+    </tbody>
+  </table>;
 
 TransferPayments.propTypes = {
   transferAmounts: PropTypes.array.isRequired

07c3a7262f8a512a1ad1fd06025e7ba4ab992ccb
sideroad <sideroad.jp@gmail.com>
2017-12-03 12:58:41 +0900

    Add transfer animation

--- src/components/TransferPayments.jsx
+++ src/components/TransferPayments.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const styles = require('../css/transfer-payments.less');
+const fa = require('../../node_modules/koiki-ui/less/fa/less/font-awesome.less');
 
 const TransferPayments = props =>
   <table
@@ -11,9 +12,19 @@ const TransferPayments = props =>
       {
         props.transferAmounts
           .filter(amount => amount.amount)
-          .map(amount =>
+          .map((amount, index) =>
             <tr key={`${amount.from}-${amount.to}`} >
               <td className={styles.from} >{amount.from}</td>
+              <td className={styles.arrow} >
+                <div className={styles.icon} >
+                  <i
+                    className={`${fa.fa} ${fa['fa-money']}`}
+                    style={{
+                      animationDelay: `${index * 0.25}s`,
+                    }}
+                  />
+                </div>
+              </td>
               <td className={styles.to} >{amount.to}</td>
               <td className={styles.amount} >￥{amount.amount.toLocaleString()}</td>
             </tr>

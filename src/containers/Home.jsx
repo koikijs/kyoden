729c12535714c341b86b7cdfc238fe67d7005a33
taka66 <taka66.iphone@gmail.com>
2017-12-17 02:10:07 +0900

    Implement loading animation for 1st screen.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
@@ -8,31 +8,41 @@ import Signature from '../components/Signature';
 import Loading from '../components/Loading';
 import uris from '../uris';
 
-const Home = (props, context) =>
-  <div>
-    <Loading
-      isActive
-    />
-    <Signature
-      lead={context.i18n.lead}
-      sublead={context.i18n.sublead}
-      eventName={props.eventName}
-      onEventChange={(values) => {
-        props.changeEvent(values);
-      }}
-      onEventSubmit={(values) => {
-        context.fetcher.event.save(values)
-          .then(({ res }) => {
-            // TODO change way to get id
-            // koiki better to fetch URL if location header has response.
-            // then the body need to pass in callback
-            console.log(res, res.headers, res.headers.values());
-            const id = res.headers.get('location').match(/\/events\/(.+)$/)[1];
-            props.push(stringify(uris.pages.event, { lang: context.lang, id }));
-          });
-      }}
-    />
-  </div>;
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  componentWillMount() {
+    this.setState({ loading: false });
+  }
+  render() {
+    return (
+      <div>
+        <Loading isActive={this.state.loading} />
+        <Signature
+          lead={this.context.i18n.lead}
+          sublead={this.context.i18n.sublead}
+          eventName={this.props.eventName}
+          onEventChange={(values) => {
+            this.props.changeEvent(values);
+          }}
+          onEventSubmit={(values) => {
+            this.context.fetcher.event.save(values)
+              .then(({ res }) => {
+                // TODO change way to get id
+                // koiki better to fetch URL if location header has response.
+                // then the body need to pass in callback
+                console.log(res, res.headers, res.headers.values());
+                const id = res.headers.get('location').match(/\/events\/(.+)$/)[1];
+                this.props.push(stringify(uris.pages.event, { lang: this.context.lang, id }));
+              });
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 Home.contextTypes = {
   lang: PropTypes.string.isRequired,

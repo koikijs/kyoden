94d7253df13fdcd103b19cbf581f3befebd50b5e
sideroad <sideroad.jp@gmail.com>
2019-07-20 14:53:09 +0900

    Add icons and titles

--- components/AddPayment.jsx
+++ components/AddPayment.jsx
@@ -1,85 +1,106 @@
 import React from 'react';
-
+import theme from '../theme';
 import Input from './koiki-ui/Input';
 import Selectbox from './koiki-ui/Selectbox';
 
 const AddPayment = (props) => {
-  const selected = props.members.find(member => member.name === props.scrooge.memberName) ||
-    props.members[0] || {
-      id: '',
-      name: '',
-    };
+  const selected = props.members.find(member => member.name === props.scrooge.memberName)
+    || props.members[0] || {
+    id: '',
+    name: '',
+  };
   return (
-    <div className="addPayment">
-      <style jsx>{`
-        .addPayment {
-          display: flex;
-          flex-direction: row;
-          width: 80%;
-          justify-content: space-between;
-          align-items: center;
-          margin-bottom: 3em;
-        }
-
-        .column {
-          width: 30%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        @media (max-width: 768px) {
-          .addPayment {
-            width: 90%;
+    <>
+      <style jsx>
+        {`
+          .title {
+            font-size: 1.25em;
+            color: ${theme.color.lightPrimary};
+            width: 80%;
           }
-        }
-      `}</style>
-      <div className="column">
-        <Selectbox
-          onSelect={option =>
-            props.onInputPayment({
+
+          .title i {
+            margin-right: 10px;
+          }
+          .addPayment {
+            display: flex;
+            flex-direction: row;
+            width: 80%;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 3em;
+          }
+
+          .column {
+            width: 30%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          @media (max-width: 768px) {
+            .title {
+              width: 90%;
+            }
+            .addPayment {
+              width: 90%;
+              flex-direction: column;
+            }
+            .column {
+              width: 100%;
+            }
+          }
+        `}
+      </style>
+      <p className="title">
+        <i className="fa fa-credit-card" />
+        Invoices
+      </p>
+      <div className="addPayment">
+        <div className="column">
+          <Selectbox
+            onSelect={option => props.onInputPayment({
               memberName: option.value,
             })
-          }
-          selected={{
-            text: selected.name,
-            value: selected.id,
-          }}
-          options={props.members.map(member => ({
-            text: member.name,
-            value: member.id,
-          }))}
-        />
-      </div>
-      <div className="column">
-        <Input
-          icon="fa-money"
-          type="number"
-          align="right"
-          placeholder="Amount"
-          value={props.scrooge.paidAmount}
-          onChange={event =>
-            props.onInputPayment({
+            }
+            selected={{
+              text: selected.name,
+              value: selected.id,
+            }}
+            options={props.members.map(member => ({
+              text: member.name,
+              value: member.id,
+            }))}
+          />
+        </div>
+        <div className="column">
+          <Input
+            icon="fa-money"
+            type="number"
+            align="right"
+            placeholder="Amount"
+            value={props.scrooge.paidAmount}
+            onChange={event => props.onInputPayment({
               paidAmount: event.target.value,
             })
-          }
-          onSubmit={props.onSubmitPayment}
-        />
-      </div>
-      <div className="column">
-        <Input
-          icon="fa-commenting-o"
-          placeholder="Reason"
-          value={props.scrooge.forWhat}
-          onChange={event =>
-            props.onInputPayment({
+            }
+            onSubmit={props.onSubmitPayment}
+          />
+        </div>
+        <div className="column">
+          <Input
+            icon="fa-commenting-o"
+            placeholder="Reason"
+            value={props.scrooge.forWhat}
+            onChange={event => props.onInputPayment({
               forWhat: event.target.value,
             })
-          }
-          onSubmit={props.onSubmitPayment}
-        />
+            }
+            onSubmit={props.onSubmitPayment}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/Members.jsx
+++ components/Members.jsx
@@ -1,33 +1,52 @@
 import React from 'react';
 
+import theme from '../theme';
 import Chips from './koiki-ui/Chips';
 
 const Members = props => (
-  <div className="members">
-    <Chips
-      focused
-      icon="fa-user"
-      placeholder="Tell me attendee name"
-      onChange={event => props.onChangeInputName(event.target.value)}
-      onSelect={props.onSelectMember}
-      onDelete={props.onDeleteMember}
-      suggests={props.suggests}
-      chips={props.members}
-    />
-    <style jsx>{`
-      .members {
-        width: 80%;
-        margin-bottom: 3em;
-        margin-top: 8em;
-      }
-
-      @media (max-width: 768px) {
-        .members {
-          width: 90%;
+  <>
+    <style jsx>
+      {`
+        .title {
+          font-size: 1.25em;
+          color: ${theme.color.lightPrimary};
         }
-      }
-    `}</style>
-  </div>
+        .title i {
+          margin-right: 10px;
+        }
+        .members {
+          width: 80%;
+          margin-bottom: 3em;
+          margin-top: 8em;
+        }
+
+        @media (max-width: 768px) {
+          .title {
+            width: 90%;
+          }
+          .members {
+            width: 90%;
+          }
+        }
+      `}
+    </style>
+    <div className="members">
+      <p className="title">
+        <i className="fa fa-users" />
+        Attendees
+      </p>
+      <Chips
+        focused
+        icon="fa-user"
+        placeholder="Tell me attendee name"
+        onChange={event => props.onChangeInputName(event.target.value)}
+        onSelect={props.onSelectMember}
+        onDelete={props.onDeleteMember}
+        suggests={props.suggests}
+        chips={props.members}
+      />
+    </div>
+  </>
 );
 
 export default Members;
